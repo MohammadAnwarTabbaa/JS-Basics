e7e23115af86bcb96525fbe5140ff44f3b8a783f
MohammadAnwarTabbaa <anwar.tabbaa94@gmail.com>
2021-05-28 15:43:09 +0300

    Step-2

---
+++ javascript-basics-01/Step_2.js
@@ -0,0 +1,9 @@
+function fullName(){
+    var name=" Mohammad Anwar ";
+    var surname= "tabbaa"; 
+    var city ="Beirut";
+    var breaks= "\r\n";
+
+    alert("Name :"+name + breaks +"Surnname : " +surname + breaks+"City : " +city);
+}
+fullName();
