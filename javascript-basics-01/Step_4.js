34e3ebdf897179455295ba1e72ec4dc5bcb7301c
MohammadAnwarTabbaa <anwar.tabbaa94@gmail.com>
2021-05-28 22:47:47 +0300

    Step_4

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,8 @@
+var names=prompt("enter your name ");
+var surname=prompt("enter your surname ");
+var city=prompt("enter your city");
+var breaks= "\r\n";
+alert("name: " + names + breaks +"surname: "+ surname + breaks+"city :" + city);
+
+
+
