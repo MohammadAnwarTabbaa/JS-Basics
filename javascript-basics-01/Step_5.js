2a3b12296dc2285b83a0393163da24b20cec1cc0
MohammadAnwarTabbaa <anwar.tabbaa94@gmail.com>
2021-05-28 23:07:28 +0300

    Step_5

---
+++ javascript-basics-01/Step_5.js
@@ -0,0 +1,6 @@
+function calculate(){
+var firstNumber = document.getElementById("first_number").value;
+var secondNumber= document.getElementById("second_number").value;
+alert("multiplication equals :"+(firstNumber*secondNumber)) ; 
+
+}
