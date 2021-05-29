70934d96cd7ee78944ffd14f805d54a3b54c70e0
MohammadAnwarTabbaa <anwar.tabbaa94@gmail.com>
2021-05-29 21:11:44 +0300

    Step 1

---
+++ javascript-basics-03/step 1/step1.js
@@ -0,0 +1,12 @@
+var image= document.getElementById("image1");
+
+
+image.addEventListener('mouseover',()=>{
+    image.style.border="solid 3px red";
+});
+
+image.addEventListener('mouseleave',()=>{
+    image.style.border="none";
+});
+
+
