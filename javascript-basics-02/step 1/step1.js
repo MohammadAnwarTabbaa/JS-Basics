552430a11aedc324606b04d737d02e7e33779128
MohammadAnwarTabbaa <anwar.tabbaa94@gmail.com>
2021-05-29 11:42:42 +0300

    Step 1

---
+++ javascript-basics-02/step 1/step1.js
@@ -0,0 +1,9 @@
+function change(){
+var image= document.querySelector("#image1");
+image.src = "images/image1_2.jpg";
+}
+ document.querySelector("#image1").addEventListener("mouseover" , change);
+
+
+ 
+
