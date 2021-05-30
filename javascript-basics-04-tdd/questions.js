431bb5cdf5ba3d5fa9d967dba03558cd0bbe4b11
MohammadAnwarTabbaa <anwar.tabbaa94@gmail.com>
2021-05-30 03:41:25 +0300

    Basics 4

--- javascript-basics-04-tdd/questions.js
+++ javascript-basics-04-tdd/questions.js
@@ -1,101 +1,142 @@
 
 const stringSize = (text) => {
-
+return text.length;
 }
 const replaceCharacterE = (text) => {
-
+ return text.replace('e',' ');
 }
 const concatString = (text1, text2) => {
-
+return text1+text2 ;
 }
 const showChar5 = (text) => {
-
+return text[4];
 }
 const showChar9 = (text) => {
-
+return text.substring(0,9);
 }
 const toCapitals = (text) => {
-
+return text.toUpperCase();
 }
 const toLowerCase = (text) => {
-
+return text.toLowerCase();
 }
 const removeSpaces = (text) => {
-
+return text.trim();
 }
 const IsString = (text) => {
-
+return (typeof(text)=="string");
 }
 
 const getExtension = (text) => {
+return text.split('.').pop();
+// return text.substring(text.length -3);
 
 }
 const countSpaces = (text) => {
-
+ return text.split(" ").length-1;
 }
 const InverseString = (text) => {
+var s ="";
+var i ;
+for(i=text.length-1 ; i>=0 ; i-- ){
+    s = s+ text[i];
+} 
+return s ;
 
 }
 
 const power = (x, y) => {
+ return Math.pow(x,y);
 
 }
 const absoluteValue = (num) => {
+    return Math.abs(num);
 
 }
 const absoluteValueArray = (array) => {
-
+    var i ;
+    var arr =[]; 
+    for (i=0 ; i<array.length ; i++ ){
+    arr.push(Math.abs(array[i]));
+    }
+    return arr ;
 }
 const circleSurface = (radius) => {
-
+return Math.ceil(Math.PI*radius*radius);
 }
+
 const hypothenuse = (ab, ac) => {
-
+return Math.sqrt((ab*ab)+(ac*ac));
 }
+
 const BMI = (weight, height) => {
-
+    var num = (weight/(height*height));
+    var ahmad =num.toFixed(2);
+    return parseFloat(ahmad);
 }
+
 
 const createLanguagesArray = () => {
-
+return ["Html","CSS","Java","PHP"];
 }
 
 const createNumbersArray = () => {
-
+return[0,1,2,3,4,5];
 }
 
 const replaceElement = (languages) => {
-
+ languages[2]="Javascript";
+ return languages;
 }
 
 const addElement = (languages) => {
 
+languages.push("Ruby");
+languages.push("Python");
+return languages;
 }
 
 const addNumberElement = (numbers) => {
-
+numbers.unshift(-2,-1);
+return numbers;
 }
 
 const removeFirst = (languages) => {
-
+ languages.shift();
+ return languages;
 }
 
 const removeLast = (languages) => {
-
+languages.pop();
+return languages;
 }
 
 const convertStrToArr = (social_arr) => {
-
+var b =social_arr.split(',');
+return b ;
 }
 
-const convertArrToStr = (languages) => {
 
+const convertArrToStr = (languages) => {
+var i ;
+var word =""; 
+for(i=0 ; i<languages.length ; i++){
+    word+=languages[i];
+    //or
+    // if(i==languages.length-1){
+    //     break;
+    // }
+    word+=","
+}
+var w=word.slice(0, -1);
+return w ; 
 }
 
 const sortArr = (social_arr) => {
 
+return social_arr.sort();
 }
 
 const invertArr = (social_arr) => {
-
+return social_arr.reverse();
 }
